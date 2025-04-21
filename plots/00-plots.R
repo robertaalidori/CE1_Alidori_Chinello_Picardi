d3286b8a155450ebc31191829cfed6ea67cb7674
luca-chinello <luca.chinello@studenti.unimi.it>
2025-04-21 22:43:22 +0200

    Created a new branch with plots ideas

--- plots/00-plots.R
+++ plots/00-plots.R
@@ -4,42 +4,40 @@ library(tidyverse)
 # Load dataset
 ESS9_recoded <- read_csv("recoded_data/ESS9_recoded.csv")
 
-#Exploratory data analysis
+# Exploratory data analysis
 # Check the structure of the dataset
 str(ESS9_recoded)
 
-# Check for missing values
-missing_values <- colSums(is.na(ESS9_recoded))
-print(missing_values)
 
-#Histogram happy
-ggplot(ESS9_recoded, aes(x = happy)) +
-  geom_histogram(binwidth = 1, fill = "blue", color = "black", alpha = 0.7) +
-  labs(title = "Histogram of Happiness Levels",
-       x = "Happiness Level",
-       y = "Frequency") +
+# Bar chart of happy_group
+ggplot(na.omit(ESS9_recoded), aes(x = fct_infreq(happy_group))) +
+  geom_bar(fill = "blue", 
+           color = "black", 
+           alpha = 0.7) +
+  labs(title = "Bar Chart of Happiness Level",
+       x = "Level of Happiness",
+       y = "Number of people") +
   theme_minimal()
 
+# Saving the plot
+ggsave("plots/happy_group_bar_chart.png", 
+       width = 8, height = 6, dpi = 300)
 
-#Histogram wealth_diff
-ggplot(ESS9_recoded, aes(x = wealth_diff)) +
-  geom_histogram(binwidth = 1, fill = "green", color = "black", alpha = 0.7) +
-  labs(title = "Histogram of Wealth Difference",
-       x = "Wealth Difference",
-       y = "Frequency") +
+
+# Bar chart of wealth
+ggplot(na.omit(ESS9_recoded), aes(x = fct_infreq(wealth))) +
+  geom_bar(fill = "green", 
+           color = "black", 
+           alpha = 0.7) +
+  labs(title = "Bar Chart of Wealth",
+       x = "Level of Wealth",
+       y = "Number of people") +
   theme_minimal()
 
+# Saving the plot
+ggsave("plots/wealth_bar_chart.png", 
+       width = 8, height = 6, dpi = 300)
 
 
 
-#regression
-regression <- lm(happy ~ wealth_diff, data = ESS9_recoded)
-summary(regression)
-# Plot the regression line
-ggplot(ESS9_recoded, aes(x = wealth_diff, y = happy)) +
-  geom_point() +
-  geom_smooth(method = "lm", se = FALSE, color = "blue") +
-  labs(title = "Regression of Happiness on Wealth Difference",
-       x = "Wealth Difference",
-       y = "Happiness Level") +
-  theme_minimal()
+
