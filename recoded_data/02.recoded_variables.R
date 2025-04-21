dcd9a0d85f9520d3a8532190dfcfafd4594bea94
luca-chinello <luca.chinello@studenti.unimi.it>
2025-04-21 22:15:05 +0200

    Cleaned the code, saved new dataset in the working directory

--- recoded_data/02.recoded_variables.R
+++ recoded_data/02.recoded_variables.R
@@ -1,32 +1,29 @@
+### Title: "02_recoded_variables" 
 
--------
-# Title: "02_recoded_variables" 
--------
--------
 # Packages
--------
-  
-  library(tidyverse)
+
+library(tidyverse)
 library(dplyr)
 library(readr)
 
--------
-# Load dataset with selected variables
--------
-  ESS9 <- read_csv("raw_data/ESS9.csv")
-  da_ESS9 <- ESS9 |> 
-    select(
-      happy, wltdffr
+# Load raw dataset
+
+ESS9 <- read_csv("raw_data/ESS9.csv")
+
+# Select relevant variables
+
+ESS9_recoded <- ESS9 |> 
+  select(
+    happy, wltdffr
     )
   
--------
-# Recode variables and add labels
-------
+
+## Recode the variables and add labels
     
 # Recode and label "happy"
-# create new variable happy_group which contains 3 values: 0–3 = Low; 4–6 = Medium; 7–10 = High
+# create a new variable happy_group which contains 3 values: 0–3 = Low; 4–6 = Medium; 7–10 = High
     
-da_ESS9 <- da_ESS9 |> 
+ESS9_recoded <- ESS9_recoded |> 
 mutate(happy_group =  case_when(
       happy >= 0 & happy <= 3 ~ "Low",
       happy >= 4 & happy <= 6 ~ "Medium",
@@ -34,18 +31,23 @@ mutate(happy_group =  case_when(
       TRUE ~ NA_character_  # for missing or invalid values
     ))
   
-view(da_ESS9$happy_group)
+view(ESS9_recoded$happy_group)
   
-# Recode and label "wltdffr"
+## Recode and label "wltdffr"
   
-# create new variable wealth which contains 3 values: 0 =Fair; -4, -3, 3, 4 = Very Unfair; -1, 1, 2, -2 = Somewhat Fair 
+# create a new variable wealth which contains 3 values: 0 = Fair; -4, -3, 3, 4 = Very Unfair; 
+# -1, 1, 2, -2 = Somewhat Fair 
   
-da_ESS9 <- da_ESS9 |> 
+ESS9_recoded <- ESS9_recoded |> 
     mutate(wealth = case_when(
       wltdffr == 0 ~ "Fair",
       wltdffr %in% c(-4, -3, 3, 4) ~ "Very Unfair",
       wltdffr %in% c(-2, -1, 1, 2) ~ "Somewhat Fair",
       TRUE ~ NA_character_
     )) 
-  
+
+view(ESS9_recoded$wealth)
+
+# Save the recoded dataset
+write_csv(ESS9_recoded, "recoded_data/ESS9_recoded.csv")
   
