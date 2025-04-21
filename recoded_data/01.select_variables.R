9e7530a78dd6fdf53f650418cd3d8d8b0c42667b
luca-chinello <luca.chinello@studenti.unimi.it>
2025-04-21 17:58:26 +0200

    erasing "-----" from script

--- recoded_data/01.select_variables.R
+++ recoded_data/01.select_variables.R
@@ -1,21 +1,15 @@
--------
 # Title: "02_variable_selected" 
--------
--------
+
 # Packages
--------
 
 library(tidyverse)
 library(dplyr)
 
--------
 # Load dataset 
--------
+
 ESS9 <- read_csv("raw_data/ESS9.csv")
 
---------
 # Select variables for data analysis 
---------
 
 # Selected variables: level of happiness and wealth fair/unfair (we need to decide which one of the two to keep)
 
