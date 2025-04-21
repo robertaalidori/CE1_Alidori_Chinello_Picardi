d3542881c1ea760718554b907ea5db5b0dec579a
roberta_alidori <roberta.alidori@unimi.it>
2025-04-21 16:22:32 +0200

    ESS9 dataset

--- raw_data/load_data.R
+++ raw_data/load_data.R
@@ -1,26 +1,19 @@
----
-  title: "Multivariate Analysis_Class Exercise 1"
-subtitle: "Raw dataset"
----
-  
-  # Set up workingplace
-  ```{r} 
-# Set up R (show the R code in the final document, hide warnings and suppress messages from packages) 
+----
+# title: "Multivariate Analysis_Class Exercise 1" ####
+# subtitle: "Raw dataset"
+----
+
+# Set up R (show the R code in the final document, hide warnings and suppress messages from packages) ####
 knitr::opts_chunk$set(echo = TRUE, warning = FALSE, message = FALSE)
-```
-```{r} 
-#Load packages
+
+#Load packages ####
 library(tidyverse)
 library(readr)
-```
-#Load data
 
-```{r} 
 
-#Load ESS9
+#Load ESS9 ####
 ESS9 <- read_csv("raw_data/ESS9.csv")
-View(ESS9)
+view(ESS9)
 
 
-```
 
