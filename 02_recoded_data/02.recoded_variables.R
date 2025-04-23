2998b6068be588737c4bbeafd25b055babc3d064
giuliapic <giulia.picardi01@gmail.com>
2025-04-23 19:52:11 +0200

    rename folder

---
+++ 02_recoded_data/02.recoded_variables.R
@@ -0,0 +1,82 @@
+### Title: "02_recoded_variables" 
+
+# Packages
+
+library(tidyverse)
+library(dplyr)
+library(readr)
+
+# Load raw dataset
+
+ESS9 <- read_csv("raw_data/ESS9.csv")
+
+# Select relevant variables
+
+ESS9_recoded <- ESS9 |> 
+  select(
+    happy, wltdffr
+    )
+  
+
+## Recode the variables and add labels
+    
+# Recode and label "happy"
+# create a new variable happy_group which contains 3 values: 0–3 = Low; 4–6 = Medium; 7–10 = High
+    
+ESS9_recoded <- ESS9_recoded |> 
+mutate(happy_group = case_when(
+      happy >= 0 & happy <= 3 ~ "Low",
+      happy >= 4 & happy <= 6 ~ "Medium",
+      happy >= 7 & happy <= 10 ~ "High",
+      TRUE ~ NA_character_  # for missing or invalid values
+    ))
+  
+## Recode and label "wltdffr"
+  
+# create a new variable wealth which contains 3 values: 0 = Fair; -4, -3, 3, 4 = Very Unfair; 
+# -1, 1, 2, -2 = Somewhat Fair 
+  
+ESS9_recoded <- ESS9_recoded |> 
+    mutate(wealth = case_when(
+      wltdffr == 0 ~ "Fair",
+      wltdffr %in% c(-4, -3, 3, 4) ~ "Very Unfair",
+      wltdffr %in% c(-2, -1, 1, 2) ~ "Somewhat Fair",
+      TRUE ~ NA_character_
+    )) 
+
+ESS9_recoded$wealth
+
+
+# Create new dataset "ESS9_clean" without NAs in variable "happy_group"
+# the new dataset should include both filters otherwise you overwrite the object, 
+# so I changed the code adding all filter in the same object and this is the result:
+
+ESS9_clean <- ESS9_recoded |>
+  filter(!is.na(happy_group), !is.na(wealth))
+
+ESS9_clean
+
+# Check "happy group"
+ESS9_clean |> 
+  count(happy_group)
+
+# Check NA values
+
+ESS9_clean |> 
+  count(wealth)
+
+
+
+# Save the recoded datasets
+
+write_csv(ESS9_recoded, "recoded_data/ESS9_recoded.csv")
+
+write_csv(ESS9_clean, "recoded_data/ESS9_clean.csv")
+
+
+#check for the final observation we have 
+
+nrow(ESS9_clean)
+
+
+  
---
+++ 04_plots/CopyOf00-plots.R
@@ -0,0 +1,107 @@
+library(dplyr)
+library(tidyverse)
+
+# Load dataset
+ESS9_clean <- read_csv("recoded_data/ESS9_clean.csv")
+
+# Exploratory data analysis
+# Check the structure of the dataset
+str(ESS9_clean)
+
+
+# Bar chart of happy_group
+ggplot(ESS9_clean, aes(x = fct_infreq(happy_group))) +
+  geom_bar(fill = "green", 
+           color = "black", 
+           alpha = 0.7) +
+  labs(title = "Bar Chart of Happiness Level",
+       x = "Level of Happiness",
+       y = "Number of people") +
+  theme_minimal()
+
+# Saving the plot
+ggsave("plots/happy_group_bar_chart.png", 
+       width = 8, height = 6, dpi = 300)
+
+
+# Bar chart of wealth discrepancy
+ggplot(ESS9_clean, aes(x = fct_infreq(wealth))) +
+  geom_bar(fill = "green", 
+           color = "black", 
+           alpha = 0.7) +
+  labs(title = "Bar Chart of Wealth",
+       x = "Level of Wealth discrepancy",
+       y = "Number of people") +
+  theme_minimal()
+
+# Saving the plot
+ggsave("plots/wealth_bar_chart.png", 
+       width = 8, height = 6, dpi = 300)
+
+
+
+<<<<<<< HEAD
+#Graph of the association between wealth discrepancies and happiness
+
+ggplot(ESS9_clean, aes(x = wealth, fill = happy_group)) +
+  geom_bar(position = "fill") +
+  labs(title = "Association between Wealth Discrepancies and Happiness",
+       x = "Wealth Discrepancy",
+       y = "Proportion of Happiness Level") +
+  scale_fill_brewer(palette = "Set4") +
+  theme_minimal()
+
+#Saving the plot
+ggsave("plots/wealth_happiness_association.png", 
+       width = 8, height = 6, dpi = 300)
+
+
+
+
+#regression with control variables to make the analysis more robust
+
+lm(happy ~ wltdffr + gndr + agea, data = ESS9)
+
+=======
+#ADD ON: regression for control variables 
+>>>>>>> 318b080af5b3b08151f05136aaac8b8613a62212
+
+control_var <- lm(happy ~ wltdffr + gndr + agea + eduyrs, data = ESS9)
+control_var
+
+<<<<<<< HEAD
+# Load necessary libraries
+library(broom)
+
+# control_var plot
+=======
+
+library(broom)
+>>>>>>> 318b080af5b3b08151f05136aaac8b8613a62212
+
+tidy(control_var, conf.int = TRUE) |> 
+  filter(term != "(Intercept)") |> 
+  ggplot(aes(x = term, y = estimate)) +
+  geom_point(size = 3) +
+  geom_errorbar(aes(ymin = conf.low, ymax = conf.high), width = 0.2) +
+  labs(
+    title = "Effect of predictors on happiness",
+    x = "Predictors",
+    y = "Estimated effect (with 95% CI)"
+  ) +
+  theme_minimal()
+
+<<<<<<< HEAD
+ggsave("plots/control_var_plot.png", 
+       width = 8, height = 6, dpi = 300)
+
+#it's clear that wealth and education have a statistically significant effect on happiness
+
+
+
+
+
+=======
+>>>>>>> 318b080af5b3b08151f05136aaac8b8613a62212
+
+
