6349075208875a2a350f67fe897cafb0cdf7199c
luca-chinello <luca.chinello@studenti.unimi.it>
2025-04-23 22:34:12 +0200

    added libraries in rmd + fixed errors in raw data

--- 01_raw_data/load_data.R
+++ 01_raw_data/load_data.R
@@ -3,16 +3,13 @@
 # subtitle: "Raw dataset"
 ----
 
-# Set up R (show the R code in the final document, hide warnings and suppress messages from packages) ####
-knitr::opts_chunk$set(echo = TRUE, warning = FALSE, message = FALSE)
-
 #Load packages ####
 library(tidyverse)
 library(readr)
 
 
 #Load ESS9 ####
-ESS9 <- read_csv("raw_data/ESS9.csv")
+ESS9 <- read_csv("01_raw_data/ESS9.csv")
 view(ESS9)
 
 
