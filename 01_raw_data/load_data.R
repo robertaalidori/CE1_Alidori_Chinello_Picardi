b0d1a17282e5fed296a1300d4cd6747371d50740
luca-chinello <luca.chinello@studenti.unimi.it>
2025-04-24 10:29:32 +0200

    last retouches

--- 01_raw_data/load_data.R
+++ 01_raw_data/load_data.R
@@ -1,7 +1,6 @@
-----
+
 # title: "Multivariate Analysis_Class Exercise 1" ####
 # subtitle: "Raw dataset"
-----
 
 #Load packages ####
 library(tidyverse)
@@ -9,8 +8,9 @@ library(readr)
 
 
 #Load ESS9 ####
-ESS9 <- read_csv("01_raw_data/ESS9.csv")
+ESS9 <- read_csv("01_raw_data/ESS9e03_2.csv")
 view(ESS9)
+
 
 
 
